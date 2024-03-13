83fada1377899a768c9a7547f2fbe1a4b027c788
shoaibRamzan42 <adnanrmzan420@gmail.com>
2024-03-13 10:57:28 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+
+
+
+function App() {
+ 
+
+  return (
+ <>
+ <div
+        
+        className="min-h-screen  gray-gradient  max-w-[100vw]  overflow-hidden "
+      >
+        <div  className="shadow-xl   max-w-[1469px]   mx-auto ">
+
+
+</div>
+</div>
+ </>
+  )
+}
+
+export default App
